ab115993686ca55c825cf87ccaa3cf09bb2d7e9b
Vindhyachal Tiwari <vindhyachaltiwari01@gmail.com>
2024-06-16 21:18:17 +0530

    updates

--- src/main/java/com/vindhyachal/inventory_service/InventoryServiceApplication.java
+++ src/main/java/com/vindhyachal/inventory_service/InventoryServiceApplication.java
@@ -17,18 +17,18 @@ public class InventoryServiceApplication {
 	@Autowired
 	private ProductRepository repository;
 
-//	@PostConstruct
-//	public void initDB() {
-//				List<Product> products = Stream.of(
-//				new Product("Laptop", "Electronics", 74999.99f, 50),
-//				new Product("Smartphone", "Electronics", 39999.99f, 100),
-//				new Product("Office Chair", "Furniture", 7999.99f, 200),
-//				new Product("Notebook", "Stationery", 99.99f, 500),
-//				new Product("Desk Lamp", "Furniture", 1999.99f, 150),
-//				new Product("Water Bottle", "Accessories", 499.99f, 300)
-//		).collect(Collectors.toList());
-//		repository.saveAll(products);
-//	}
+	@PostConstruct
+	public void initDB() {
+				List<Product> products = Stream.of(
+				new Product("Laptop", "Electronics", 74999.99f, 50),
+				new Product("Smartphone", "Electronics", 39999.99f, 100),
+				new Product("Office Chair", "Furniture", 7999.99f, 200),
+				new Product("Notebook", "Stationery", 99.99f, 500),
+				new Product("Desk Lamp", "Furniture", 1999.99f, 150),
+				new Product("Water Bottle", "Accessories", 499.99f, 300)
+		).collect(Collectors.toList());
+		repository.saveAll(products);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
